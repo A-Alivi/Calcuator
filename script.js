165be1ab9cd5004d5c6412c5a3a05648d5759a62
A-Alivi <hakeemalivi5@gmail.com>
2025-07-31 15:22:31 +0500

    removed commented code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let str1 = "";
 let result = ""
 
 
-    
+
 function handleClick(id)
 {
     str1  = str1+ id;
@@ -46,80 +46,5 @@ function clearScreen()
 }
 
 
-// $(document).ready(function()
-// {
-
-    
-//     var str1 = ""
-//     $("#one").click(function(){
-
-//         $(".screen").text("1")
-//         str1 += $(".screen").text()
-//     })
-
-//         $("#two").click(function(){
-//         $(".screen").text("2")
-//         str1.concat("Abdul")
-//         console.log(str1.length)
-
-//     })
-//         $("#three").click(function(){
-//         $(".screen").text("3")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-
-//     })
-//         $("#four").click(function(){
-//         $(".screen").text("4")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#five").click(function(){
-//         $(".screen").text("5")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#six").click(function(){
-//         $(".screen").text("6")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#seven").click(function(){
-//         $(".screen").text("7")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#eight").click(function(){
-//         $(".screen").text("8")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#nine").click(function(){
-//         $(".screen").text("9")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#10").click(function(){
-//         $(".screen").text("10")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#equal").click(function(){
-//         $(".screen").text("=")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#add").click(function(){
-//         $(".screen").text("+")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-//         $("#zero").click(function(){
-//         $(".screen").text("0")
-//         str1.concat($(".screen").text())
-//         console.log(str1.length)
-//     })
-
-// })
 
 
